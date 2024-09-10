29a3dd9bfc326ca0e008c523a99183775f7c6fd5
taurn kale <tkumarkale@gmail.com>
2024-09-10 12:32:45 +0530

    hello *3

--- src/App.jsx
+++ src/App.jsx
@@ -6,53 +6,51 @@ const App = () => {
     lname: ''
   });
 
-  const [second, setsecond] = useState('');
+  const [second, setsecond] = useState([]);
 
   const hello = (e) => {
     const value = e.target.value;
     const name = e.target.name;
-    setfirst((pre) => {
-      if (name === 'fname') {
-        return {
-          fname: value,
-          lname: pre.lname
-        };
-      } else if (name === 'lname') {
-        return {
-          fname: pre.fname,
-          lname: value
-        };
-      }
-      return pre;
-    });
+    setfirst((pre) => ({
+      ...pre,
+      [name]: value
+    }));
   };
+
+  console.log(first);
 
   const letsgo = (e) => {
     e.preventDefault();
     const thousand = `${first.fname} and ${first.lname}`;
-    setsecond(thousand);
+    setsecond((pre) => [...pre, thousand]);
   };
+
+  console.log(second);
 
   return (
     <div>
       <form onSubmit={letsgo}>
         <input 
           type="text" 
-               className="border border-red-600"
+          className="border border-red-600"
           onChange={hello} 
           value={first.fname} 
           name='fname' 
         />
         <input 
-        className="border border-red-600"
+          className="border border-red-600"
           type="text" 
           onChange={hello} 
           value={first.lname} 
           name='lname' 
         />
         <button type="submit">Click</button>
-        <p>{second}</p>
       </form>
+      <div>
+        {second.map((ele, index) => (
+          <p key={index}>{ele}</p>
+        ))}
+      </div>
     </div>
   );
 };
