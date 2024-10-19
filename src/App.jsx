bfda9ab2c74e8ad89875242807277562dd6eb5d2
taurn kale <tkumarkale@gmail.com>
2024-10-19 12:29:28 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
+import React from 'react'
 
-import React, { useRef } from 'react';
-
-function App() {
-  // Creating refs for the input and output elements
-  const inputRef = useRef();
-  const outputRef = useRef();
-
-  // Function to handle button click and display inputted data
-  const showData = () => {
-    const inputValue = inputRef.current.value;
-    outputRef.current.textContent = `You entered: ${inputValue}`;
-  };
-
+const App = () => {
   return (
     <div>
-      {/* Input field with ref */}
-      <input type="text" ref={inputRef} placeholder="Type something..." />
-
-      {/* Button to trigger the showData function */}
-      <button onClick={showData}>Show Data</button>
-
-      {/* Output area with ref to display the entered data */}
-      <div ref={outputRef}></div>
-      <div>hello taurn kale</div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
