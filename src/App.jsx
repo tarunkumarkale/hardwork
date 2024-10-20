d8da8e7222bd176663679ae2c08cf7d8ef07427e
taurn kale <tkumarkale@gmail.com>
2024-10-20 14:51:20 +0530

    connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import react, { useState } from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
-const App=()=>{
+const App = () => {
 
+  const [first, setFirst] = useState('')
+  const [second, setSecond] = useState([])
 
-const [first,setfirst]=useState()
+  const handleInputChange = (e) => {
+    let task = e.target.value
+    setFirst(task)
+  }
 
-const data=[
-  {name:'jhon',age:'23',Id:'0'},
-  {name:'kale',age:'43',Id:'1'},
-  {name:'left',age:'34',Id:'2'},
-  {name:'right',age:'88',Id:'3'},
-]
+  const addTask = () => {
+    setSecond((prev) => [...prev, first])
+    setFirst('')
+  }
 
-  return(
+  const deleteTask = (index) => {
+    let remainingTasks = second.filter((_, i) => i !== index)
+    setSecond(remainingTasks)
+  }
 
+  return (
     <div>
-      {data.map((Element,index,Array)=> {<Card  {...data} />} )}
+      <input type="text" value={first} onChange={handleInputChange} />
+      <button onClick={addTask}>click</button>
+
+      {second.map((ele, index) => 
+        <Card key={index} task={ele} del={() => deleteTask(index)} />
+      )}
     </div>
-   
   )
 }
+
 export default App
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
-      
+      <ul>
+        <li>
+          {props.task} 
+          <button onClick={props.del}>Delete</button>
+        </li>
+      </ul>
     </div>
   )
 }
